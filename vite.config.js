e7b29c3ca511260e844190a02ce4bbdeb25dc7cc
lhernandez66@ivytech.edu <lhernandez66@ivytech.edu>
2025-04-15 22:06:31 -0400

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React/", 
+  base: "https://lhernandez66.github.io/React/", 
   plugins: [react()],
   assetsInclude: ['**/*.JPG']
 })
